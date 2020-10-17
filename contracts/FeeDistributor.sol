3ba1ae2d27b26edc8422aa6b2bd64c0b66220dc1
Justin Goro <justingoro@protonmail.com>
2020-10-17 16:09:38 +0200

    Liquid vault interface fleshed out

--- contracts/FeeDistributor.sol
+++ contracts/FeeDistributor.sol
@@ -3,23 +3,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FeeDistributor is Ownable {
 
-    feeProportion public liquidVault; 
-    feeProportion public nftVault; 
-    feeProportion public marketingVault;
-    feeProportion public devVault; 
-    feeProportion public devVault2;
-
-    struct feeProportion {
-        address recipient;
-        uint feeProportion;
+    function setLiquidShare (uint share) public onlyOwner {
+        liquidVaultShare = share;
     }
 
-    function setLiquidVault (address v, address p) public    onlyOwner {}   
-    function setNftVault   (address v, address p)     public onlyOwner {}     
-    function setMarketingVault  (address v, address p) public onlyOwner {}      
-    function setDevVault     (address v, address p)  public   onlyOwner {}   
-    function setFallback     (address v, address p)  public  onlyOwner {}   
-
+    uint public liquidVaultShare; //percentage between 0 and 100
 
     function distributeFees() public {
 
--- contracts/LiquidVault.sol
+++ contracts/LiquidVault.sol
@@ -1,10 +1,34 @@
 pragma solidity ^0.6.0;
+import "@openzeppelin/contracts/access/Ownable.sol"; 
+contract LiquidVault is Ownable {
 
-contract LiquidVault {
-    function purchaseLP() public payable{
+    /*
+        A user can hold multiple locked LP batches. Each batch takes 30 days to incubate
+    */
+    struct LPbatch {
+        address holder;
+        uint256 amount;
+        uint256 timeStamp;
+    }
+
+    uint public stakeDuration = 30 days;
+    //Front end can loop through this and inspect if enough time has passed
+    mapping(address => LPbatch[]) public LPstakes;
+
+    address public HCORE;  //use to get uni token pair
+    
+    function configure (uint duration, address hcore) public {
+            stakeDuration = duration;
+            HCORE = hcore;
+    }
+
+    //send eth to match with HCORE tokens in LiquidVault
+    function purchaseLP() public payable {
 
     }
-    function claimLP () public {
+
+    //pops latest LP if older than period
+    function claimLP() public {
 
     }
 }
