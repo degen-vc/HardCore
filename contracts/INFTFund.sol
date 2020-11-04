5d9ffc6d398ce5e4516307e9361529626220059c
piron <paul.bolhar@gmail.com>
2020-11-04 20:19:51 +0200

    fix comp interface err.

--- contracts/INFTFund.sol
+++ contracts/INFTFund.sol
@@ -11,9 +11,6 @@ interface INFTFund {
     // @dev sell HCORE token for ETH on uniswap, with limit amount
     function sellToken(uint256 amountIn) external;
 
-    // @dev claim eth for the owner
-    function withdraw() external;
-
     // @dev claim token for the owner
     function withdrawToken(address erc20Token) external;
 }
