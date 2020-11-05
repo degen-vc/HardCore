bdec4d30f6e9d0f132902c36b18bd7c7e546bc69
piron <paul.bolhar@gmail.com>
2020-11-05 18:58:57 +0200

    add burning to fee distrib

--- contracts/FeeDistributor.sol
+++ contracts/FeeDistributor.sol
@@ -1,16 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.12;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./facades/ERC20Like.sol";
 import "./INFTFund.sol";
 
 contract FeeDistributor is Ownable {
+    using SafeMath for uint;
+
     ERC20Like public hcore;
 
     struct FeeRecipient {
         address liquidVault;
         address dev;
         uint256 liquidVaultShare; //percentage between 0 and 100
+        uint256 burnPercentage;
     }
 
     FeeRecipient public recipients;
@@ -29,31 +33,61 @@ contract FeeDistributor is Ownable {
         address hardCore,
         address liquidVault,
         address dev,
-        uint256 liquidVaultShare
+        uint256 liquidVaultShare,
+        uint256 burnPercentage
     ) public onlyOwner {
         require(
             liquidVaultShare <= 100,
             "HARDCORE: liquidVault share must be between 0 and 100"
         );
+        require(
+            burnPercentage <= 100,
+            "HARDCORE: burnPercentage share must be between 0 and 100"
+        );
+        require(
+            liquidVaultShare.add(burnPercentage) <= 100,
+            "HARDCORE: liquidVault + burnPercentage incorrect sets"
+        );
+
         hcore = ERC20Like(hardCore);
         recipients.liquidVault = liquidVault;
         recipients.dev = dev;
         recipients.liquidVaultShare = liquidVaultShare;
+        recipients.burnPercentage = burnPercentage;
         initialized = true;
     }
 
     function distributeFees() public seeded {
         uint256 balance = hcore.balanceOf(address(this));
-        //overflow not possible because balance is capped low
-        uint256 liquidShare = (recipients.liquidVaultShare * balance) / 100;
+
+        require(balance > 100, "HARDCORE: low token balance");
+
+        uint256 liquidShare;
+        uint256 burningShare;
+
+        if (recipients.liquidVaultShare > 0) {
+            liquidShare = recipients.liquidVaultShare.mul(balance).div(100);
+
+            require(
+                hcore.transfer(recipients.liquidVault, liquidShare),
+                "HARDCORE: transfer to liquidVault failed"
+            );
+        }
+
+        if (recipients.burnPercentage > 0) {
+            burningShare = recipients.burnPercentage.mul(balance).div(100);
+            (bool success, ) = address(hcore).delegatecall(
+                abi.encodeWithSelector(hcore.transferFrom.selector, burningShare)
+            );
+
+            require(success, "HARDCORE: token burn fail");
+        }
+
         require(
-            hcore.transfer(recipients.liquidVault, liquidShare),
-            "HARDCORE: transfer to liquidVault failed"
-        );
-        INFTFund(recipients.dev).deposit(balance - liquidShare);
-        require(
-            hcore.transfer(recipients.dev, balance - liquidShare),
+            hcore.transfer(recipients.dev, balance.sub(liquidShare).sub(burningShare)),
             "HARDCORE: transfer to dev failed"
         );
+
+        INFTFund(recipients.dev).deposit(balance.sub(liquidShare).sub(burningShare));
     }
 }
--- contracts/facades/ERC20Like.sol
+++ contracts/facades/ERC20Like.sol
@@ -1,36 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.1;
 
-abstract contract ERC20Like {
-    function totalSupply() external view virtual returns (uint256);
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-    function balanceOf(address account) external view virtual returns (uint256);
+abstract contract ERC20Like is IERC20 {
+    function totalSupply() external view virtual override returns (uint256);
+
+    function balanceOf(address account) external view virtual override returns (uint256);
 
     function transfer(address recipient, uint256 amount)
         external
         virtual
+    override
         returns (bool);
 
     function allowance(address owner, address spender)
         external
         view
         virtual
+    override
         returns (uint256);
 
     function approve(address spender, uint256 amount)
         external
         virtual
+    override
         returns (bool);
 
     function transferFrom(
         address sender,
         address recipient,
         uint256 amount
-    ) external virtual returns (bool);
+    ) external virtual override returns (bool);
 
     function name() public view virtual returns (string memory);
 
     function symbol() public view virtual returns (string memory);
 
     function decimals() public view virtual returns (uint8);
+
+    function burn(uint256 value) public virtual;
 }
